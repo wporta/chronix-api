37531e5c123b32e64fdcbcec59dd7dcc01105c37
Wilfredo Porta <wporta@gmail.com>
2025-06-13 18:17:17 -0600

    Initial commit.

---
+++ src/main/java/org/wporta/chronix_api/ChronixApiApplication.java
@@ -0,0 +1,13 @@
+package org.wporta.chronix_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChronixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChronixApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wporta/chronix_api/ChronixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wporta.chronix_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronixApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
